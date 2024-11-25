9b90abdad91e7e366cbe447fc2d739ef8e2899b0
nikhil2k3 <sainikhil6803@gmail.com>
2024-11-25 19:01:48 +0530

    modified

---
+++ temp.js
@@ -0,0 +1,46 @@
+db.products.insertMany(
+    [
+    {
+        "name": "Product5",
+        "description":"This is the desc of prod5",
+        "price":30,
+        "url":"https://picsum.photos/id/5/200/200"
+    },
+    {
+        "name": "Product6",
+        "description":"This is the desc of prod6",
+        "price":90,
+        "url":"https://picsum.photos/id/6/200/200"
+    },
+   
+    {
+        "name": "Product1",
+        "description":"This is the desc of prod5",
+        "price":10,
+        "url":"https://picsum.photos/id/1/200/200"
+    },
+    {
+        "name": "Product2",
+        "description":"This is the desc of prod5",
+        "price":20,
+        "url":"https://picsum.photos/id/2/200/200"
+    }
+    {
+        "name": "Product3",
+        "description":"This is the desc of prod5",
+        "price":10,
+        "url":"https://picsum.photos/id/3/200/200"
+    },
+    {
+        "name": "Product4",
+        "description":"This is the desc of prod5",
+        "price":10,
+        "url":"https://picsum.photos/id/4/200/200"
+    },
+
+   
+   
+
+
+]
+);
